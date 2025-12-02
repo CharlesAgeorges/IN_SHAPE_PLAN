65bc7e2013c1ebd73e2c6b1f12faab400a90407a
Brendan Le Sage <byrdlesage@gmail.com>
2025-12-02 14:27:46 +0100

    message_controller and view wip

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,30 @@
+class MessagesController < ApplicationController
+
+  SYSTEM_PROMPT = "You a professional fitness coach with years of experience\n\nI am a coachee of yours, wanting to get in shaoe\n\ngive me a training plan based on the data i gave you about myself and my goals\n\n"
+
+  def create
+    @chat = current_user.chats.find(params[:chat_id])
+    @user_profile = @chat.user_profile
+
+    @message = Message.new(message_params)
+    @message.chat = @chat
+    @message.role = "user"
+
+    if @message.save
+      ruby_llm_chat = RubyLLM.chat
+      response = ruby_llm_chat.with_instructions(SYSTEM_PROMPT).ask(@message.content)
+      Message.create(role: "assistant", content: response.content, chat: @chat)
+
+      redirect_to chat_path(@chat)
+    else
+      render "chats/show", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def message_params
+  params.require(:message).permit(:content)
+  end
+
+end
