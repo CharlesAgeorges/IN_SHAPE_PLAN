412d1d4c433fdf44d1d0e27b863db5f6af3497ed
Charles AGEORGES <charles.ageorges@gmail.com>
2025-12-02 14:13:20 +0100

    chat_controller

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,24 +1,27 @@
 class ChatsController < ApplicationController
   # before_action :authenticate_user!
   before_action :set_chat, only: [:show]
+  before_action :profile_exist
 
   def index
     @chats = current_user.chats.order(created_at: :desc)
   end
 
   def show
+    # redirect_to chats_path, alert: "Not authorized" and return unless @chat.user == current_user
+
     @message = Message.new
     @messages = @chat.messages.order(:created_at)
   end
 
   def new
     @chat = Chat.new
-    @profiles = current_user.user_profile
   end
 
   def create
     @chat = Chat.new(chat_params)
     @chat.user = current_user
+    @chat.user_profile = current_user.user_profile
 
     if @chat.save
       redirect_to @chat, notice: "Chat created!"
@@ -33,7 +36,14 @@ class ChatsController < ApplicationController
     @chat = Chat.find(params[:id])
   end
 
+  # Assure qu'un user ne crée pas de chat sans profil fitness
+  def profile_exist
+    return if current_user.user_profile.present?
+
+    redirect_to new_user_profile_path, alert: "Please create your fitness profile first."
+  end
+
   def chat_params
-    params.require(:chat).permit(:title, :user_profile_id)
+    params.require(:chat).permit(:title)
   end
 end
