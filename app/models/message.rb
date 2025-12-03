1b55a6997fa2b6da1c7d15477542f5a41e3e9357
Steve Morin <steve.morin@outlook.fr>
2025-12-03 12:15:33 +0100

    chatmemory

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,8 +2,17 @@ class Message < ApplicationRecord
   belongs_to :chat
   validates :role, :content, presence: true
 
+  MAX_USER_MESSAGES = 10
+
+  validate :user_message_limit, if: -> { role == "user" }
+
 
     private
 
+    def user_message_limit
+      if chat.messages.where(role: "user").count >= MAX_USER_MESSAGES
+        errors.add(:content, "You can only send #{MAX_USER_MESSAGES} messages per chat.")
+    end
+  end
   # def user_message limit
 end
