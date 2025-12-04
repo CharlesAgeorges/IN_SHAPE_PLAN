6e2c65880803e658c680441c09a14120c8f20a3c
Charles AGEORGES <charles.ageorges@gmail.com>
2025-12-04 15:02:48 +0100

    front_modifs

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,13 +6,11 @@ class Message < ApplicationRecord
 
   validate :user_message_limit, if: -> { role == "user" }
 
+  private
 
-    private
+  def user_message_limit
+    return unless chat.messages.where(role: "user").count >= MAX_USER_MESSAGES
 
-    def user_message_limit
-      if chat.messages.where(role: "user").count >= MAX_USER_MESSAGES
-        errors.add(:content, "You can only send #{MAX_USER_MESSAGES} messages per chat.")
-    end
+    errors.add(:content, "You can only send #{MAX_USER_MESSAGES} messages per chat.")
   end
-  # def user_message limit
 end
